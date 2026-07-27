3605ef8f13628ff3df61aac9e3f0396e52e0f082
iki1uc <entertrainer@web.de>
2026-07-27 08:24:18 +0200

    Refactor IMPORT_RECHNER to use PIPE12

--- rechner.js
+++ rechner.js
@@ -1,36 +1,5 @@
-import { SYS_FIND_KOOP_POOK } from "../sys/SYS.js";
+import { PIPE12 } from "./PIPE.js";
 
 export function IMPORT_RECHNER(input) {
-
-    // VECTOR.EXP global
-    const vector = window.vectorEXP;
-
-    // RESPO CSV laden
-    const respoList = window.RESPO_LIST || [];
-
-    // SYS entscheidet KOOP + POOK
-    const match = SYS_FIND_KOOP_POOK(respoList);
-
-    // PIPE‑4 Verarbeitung
-    const step1 = input;
-    const step2 = {
-        qi: vector.qi(input),
-        iqq: vector.iqq(input),
-        octa: vector.octa(input),
-        pipe: `pipe${(input % 12) || 12}`
-    };
-    const step3 = match.selected;
-    const step4 = {
-        sys: `SYS-${match.selected.key}`,
-        valid: match.koop && match.pook
-    };
-
-    return {
-        PIPE4: {
-            input: step1,
-            vector: step2,
-            respo: step3,
-            sys: step4
-        }
-    };
+    return PIPE12(input);
 }
